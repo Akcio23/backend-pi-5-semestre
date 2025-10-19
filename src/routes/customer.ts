63a851323fd71c0f97ee120e5eef9dc2138cd645
Paulo Henrique de Andrade <paulohenrique.andradefilho@gmail.com>
2025-10-18 23:10:05 -0300

    feature: create customer routes with PATCH endpoint

---
+++ src/routes/customer.ts
@@ -0,0 +1,13 @@
+import { Router } from "express";
+import CustomerController from "../Controllers/customerController.ts";
+
+const customer = Router();
+const customerController = new CustomerController();
+
+// Precisa adicionar o middleware depois
+customer.patch(
+  "/customer",
+  customerController.updateCustomer.bind(customerController)
+);
+
+export default customer;
