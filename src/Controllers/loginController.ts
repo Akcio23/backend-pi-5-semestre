dee28755732849e53139aa683f616162d440e9a4
akcio23 <dev.akcio@gmail.com>
2025-10-22 14:59:48 -0300

    test

--- src/Controllers/loginController.ts
+++ src/Controllers/loginController.ts
@@ -14,8 +14,12 @@ class loginController {
 
     const user = await User.findOne({ email });
 
-    if (!user) {
-      return res.status(404).json({ message: 'User not found' });
+    if (    !user       ) {
+
+
+
+
+      return res.status(404).json({ message: "User not found" });
     }
     const isPasswordValid = await bcrypt.compare(password, user.password);
 
